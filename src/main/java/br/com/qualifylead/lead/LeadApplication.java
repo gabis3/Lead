91f8e0cfd23551cee068abe51121e1d9501c1484
Savio <myap@outlook.com.br>
2022-12-14 13:08:53 -0300

    projeto atualzado

--- src/main/java/br/com/qualifylead/lead/LeadApplication.java
+++ src/main/java/br/com/qualifylead/lead/LeadApplication.java
@@ -6,10 +6,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import org.springframework.context.annotation.Bean;
-
+import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.FixedLocaleResolver;
-
+@Controller
 @SpringBootApplication
 public class LeadApplication {
 
--- src/main/java/br/com/qualifylead/lead/WebSecurityConfig.java
+++ src/main/java/br/com/qualifylead/lead/WebSecurityConfig.java
@@ -8,8 +8,6 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 
@@ -23,18 +21,33 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
 		http
-			.authorizeRequests()
-//			    .antMatchers("/", "/home").permitAll()
-				.anyRequest().authenticated()
-			.and()
-			.formLogin(form -> form
-					.loginPage("/login")
-					.defaultSuccessUrl("/home", true)
-					.permitAll()
-				)
-				.logout((logout) -> logout.permitAll());
+		.authorizeRequests()
+//		    .antMatchers("/", "/home").permitAll()
+	    .antMatchers("/").permitAll()
+			.anyRequest().authenticated();
+			
+	        http
+            .formLogin()
+            .loginPage("/login")
+            .defaultSuccessUrl("/home")
+            .failureUrl("/login?error")
+            .permitAll()
+            .and()
+            .logout()
+            .invalidateHttpSession(true)
+            .logoutSuccessUrl("/login?logout")
+            .permitAll();
+			
+//		.and()
+//		.formLogin(form -> form
+//				.loginPage("/login")
+//				.defaultSuccessUrl("/home", true)
+//				.permitAll()
+//			)
+//			.logout((logout) -> logout.permitAll());
 
-	}
+}
+
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception{
